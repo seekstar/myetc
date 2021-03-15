29b023c5810e7feed27ebe3a29e17041a272d8eb
Jiansheng QIU <632863986@qq.com>
2021-03-15 20:48:59 +0800

    Fix symlink of encrypt.sh and decipher.sh

--- public.sh
+++ public.sh
@@ -13,11 +13,12 @@ cd ../..
 
 cd config/Templates
 bash deploy.sh
+cd ../..
 
 mkdir -p ~/software
-ln -s $(pwd)/software//encrypt/encrypt.sh ~/software/
+ln -s $(pwd)/software/encrypt/encrypt.sh ~/software/
 chmod +x ~/software/encrypt.sh
-ln -s $(pwd)/software//encrypt/decipher.sh ~/software/
+ln -s $(pwd)/software/encrypt/decipher.sh ~/software/
 chmod +x ~/software/decipher.sh
 
 git remote remove origin
