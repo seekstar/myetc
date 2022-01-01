18cb610ff0ebf78ed910ec89832e63a662824206
Jiansheng QIU <632863986@qq.com>
2022-01-01 11:32:48 +0800

    Use jianshengqiu.cs@gmail.com

--- public.sh
+++ public.sh
@@ -1,5 +1,5 @@
 # Since this repository has been cloned, git should have been installed.
-git config --global user.email "632863986@qq.com"
+git config --global user.email "jianshengqiu.cs@gmail.com"
 git config --global user.name "Jiansheng QIU"
 
 THISDIR=$(pwd)
