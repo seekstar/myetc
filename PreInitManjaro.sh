3ed6fe6a2493e03b2b08e950c4a767ace5e3cce1
Jiansheng QIU <jianshengqiu.cs@gmail.com>
2022-01-12 22:55:41 +0800

    Do not set archlinuxcn at this time

--- PreInitManjaro.sh
+++ PreInitManjaro.sh
@@ -1,12 +1,4 @@
 sudo pacman-mirrors -i -c China -m rank
 #sudo pacman -Syy
 
-sudo bash -c "echo -e \"[archlinuxcn]\nSigLevel = Never\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\\\$arch\" >> /etc/pacman.conf"
-sudo pacman -Syyu
-
-sudo pacman -S yay
-#yay --save --aururl https://aur.mirrors.ustc.edu.cn
-yay --save --aururl https://aur.tuna.tsinghua.edu.cn
-
 bash pacman.sh
-
