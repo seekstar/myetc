d1fe39bf869da0543f81cd9cafaa583b3216dd0a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-01-20 07:38:08 +0000

    Qiu

--- public.sh
+++ public.sh
@@ -1,6 +1,6 @@
 # Since this repository has been cloned, git should have been installed.
 git config --global user.email "jianshengqiu.cs@gmail.com"
-git config --global user.name "Jiansheng QIU"
+git config --global user.name "Jiansheng Qiu"
 
 echo 'export EDITOR=vim' >> ~/.bashrc
 
