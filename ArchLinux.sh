b63fa208984e705233d296678b2dc92926e4d84d
Jiansheng QIU <jianshengqiu.cs@gmail.com>
2022-02-13 11:20:27 +0800

    reflector.timer

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -1,4 +1,9 @@
-sudo reflector --country China --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+#sudo reflector --country China --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo mv /etc/xdg/reflector/reflector.conf /etc/xdg/reflector/reflector.conf.$(date +%Y%m%d%X%N)
+sudo cp config/reflector.conf /etc/xdg/reflector/
+sudo systemctl enable reflector.timer
+sudo systemctl start reflector.timer
+
 sudo pacman -S wget
 bash pacman.sh
 bash PreInitLinux.sh
