7fb84bca202bf7f6b26088ec53572e751f0b27d3
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-03-04 14:20:07 +0800

    mkdir ~/.ssh

--- extract_key.sh
+++ extract_key.sh
@@ -4,6 +4,10 @@ extract() {
 	bash ~/software/decipher.sh id_rsa_git.aes256 ~/.ssh
 }
 
+if [ ! -d ~/.ssh/ ]; then
+	mkdir ~/.ssh
+	chmod 700 ~/.ssh
+fi
 if [ ! -r ~/.ssh/id_rsa_git ]; then
 	echo Please enter the password:
 	extract
