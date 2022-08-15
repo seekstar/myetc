deb5ae0143f6517e5036edaf24eb0f7e719a4c4d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-08-15 13:59:35 +0000

    NixOS

---
+++ config/configuration.nix
@@ -0,0 +1,93 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+	imports = [
+		# Include the results of the hardware scan.
+		./hardware-configuration.nix
+	];
+
+	nix.binaryCaches = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.searchstar = {
+		isNormalUser = true;
+		extraGroups = [
+			"wheel" # Enable ‘sudo’ for the user.
+			"networkmanager"
+		]; 
+	};
+
+	# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.grub.useOSProber = true;
+
+	# Set your time zone.
+	time.timeZone = "China/Shanghai";
+
+	# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+	# Per-interface useDHCP will be mandatory in the future, so this generated config
+	# replicates the default behaviour.
+	networking.useDHCP = false;
+	networking.interfaces.enp3s0.useDHCP = true;
+
+	networking.networkmanager.enable = true;
+
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+	# Enable the X11 windowing system.
+	services.xserver.enable = true;
+
+
+	# Enable the GNOME Desktop Environment.
+	services.xserver.displayManager.gdm.enable = true;
+	services.xserver.desktopManager.gnome.enable = true;
+  
+
+	# Enable CUPS to print documents.
+	services.printing.enable = true;
+
+	# Enable sound.
+	sound.enable = true;
+	hardware.pulseaudio.enable = true;
+
+	# Enable touchpad support (enabled default in most desktopManager).
+	services.xserver.libinput.enable = true;
+
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		vim # The Nano editor is also installed by default.
+		wget
+		firefox
+		git
+		openssl
+		clash
+		element-desktop
+		iotop
+		jnettop
+		gnome.gnome-system-monitor
+	];
+
+	# List services that you want to enable:
+
+	# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
+	services.flatpak.enable = true;
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "21.11"; # Did you read the comment?
+
+}
+
