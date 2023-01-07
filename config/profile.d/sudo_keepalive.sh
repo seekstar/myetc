61361e5c65a5ec015d7454d272b93d10bb12533f
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-01-07 17:01:59 +0800

    sudo_keepalive.sh

---
+++ config/profile.d/sudo_keepalive.sh
@@ -0,0 +1,9 @@
+function sudo_keepalive {
+	sudo -v || exit $?
+	while true; do
+		sudo -nv 
+		sleep 1
+	done &
+	trap "kill $!" EXIT
+}
+# Reference: https://serverfault.com/questions/266039/temporarily-increasing-sudos-timeout-for-the-duration-of-an-install-script
