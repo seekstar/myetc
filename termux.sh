77a8336ed44f9f6c1ebc1b8d787f800d649ef760
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-01-13 10:56:19 +0800

    termux sshd

--- termux.sh
+++ termux.sh
@@ -2,3 +2,11 @@ bash public.sh
 cd software/small_tools
 bash deploy.sh
 apt install vim
+
+apt install openssh
+# sshd. Then will listen to 8022
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
+touch authorized_keys
+chmod 600 authorized_keys
