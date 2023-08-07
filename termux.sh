2e019cea04020e6487eab62f7f11fd4ae6c25ffc
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-07 12:14:23 +0800

    termux nuc-ssh

--- termux.sh
+++ termux.sh
@@ -10,3 +10,14 @@ chmod 700 .ssh
 cd .ssh
 touch authorized_keys
 chmod 600 authorized_keys
+
+#https://blog.csdn.net/YiBYiH/article/details/127294017
+pkg install termux-services
+# Restart termux to take effect
+
+mkdir -p $PREFIX/var/service/nuc-ssh
+cat > $PREFIX/var/service/nuc-ssh/run <<EOF
+#!/data/data/com.termux/files/usr/bin/sh
+ssh -NR 6004:localhost:8022 portuser@nuc.seekstar.top -p 8789
+EOF
+
