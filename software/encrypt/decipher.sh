40851a9160438c8c9dcaa70fc4c73907b2759bca
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-31 18:27:09 +0800

    echo usage

--- software/encrypt/decipher.sh
+++ software/encrypt/decipher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#echo Please enter the password:
-#read -s psswd
+if [ ! $1 ]; then
+	echo Usage: $0 input-file [output-dir,default:current dir]
+	exit 1
+fi
 if [ $2 ]; then
 	#cat $1 | openssl aes256 -d -k "$psswd" | tar -zx -C $2
 	cat $1 | openssl aes256 -pbkdf2 -salt -md sha256 -d | tar zx -C $2
@@ -9,4 +11,3 @@ else
 	#cat $1 | openssl aes256 -d -k "$psswd" | tar -zx
 	cat $1 | openssl aes256 -pbkdf2 -salt -md sha256 -d | tar zx
 fi
-
