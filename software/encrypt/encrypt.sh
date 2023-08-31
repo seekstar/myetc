951e97d95bc0e1f41d0807110573baf886571f3a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-31 18:35:05 +0800

    Fix encrypt.sh

--- software/encrypt/encrypt.sh
+++ software/encrypt/encrypt.sh
@@ -4,7 +4,9 @@ if [ ! $2 ]; then
 	echo Usage: $0 input-path output-file
 	exit 1
 fi
-tar -zcf - $(basename $1) | openssl aes256 -pbkdf2 -salt -md sha256 > $2
+ori=$(pwd)
+cd $(dirname $1)
+tar -zcf - $(basename $1) | openssl aes256 -pbkdf2 -salt -md sha256 > $ori/$2
 #tar -zcf - $(basename $1) | openssl aes256 -salt -k "$psswd" > $ori/$2
 #openssl aes256 -e -pbkdf2 < a.txt > a.txt.aes256
 
