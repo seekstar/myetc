4edf4e6928f5f5bb17244b0c20576d1a96d9b21d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-31 18:23:19 +0800

    echo usage

--- software/encrypt/encrypt.sh
+++ software/encrypt/encrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $2 ]; then
-	tar -zcf - $(basename $1) | openssl aes256 -pbkdf2 -salt -md sha256 > $2
-else
-	echo Must specify the name of output file
+if [ ! $2 ]; then
+	echo Usage: $0 input-path output-file
+	exit 1
 fi
+tar -zcf - $(basename $1) | openssl aes256 -pbkdf2 -salt -md sha256 > $2
 #tar -zcf - $(basename $1) | openssl aes256 -salt -k "$psswd" > $ori/$2
 #openssl aes256 -e -pbkdf2 < a.txt > a.txt.aes256
 
