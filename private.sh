4e5b454faf0c2e5852df1fc5cadb76ecd6f22db6
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-02 00:43:21 +0800

    Move code to tools

--- private.sh
+++ private.sh
@@ -8,27 +8,7 @@ chmod 700 ~/.ssh
 bash helper/extract_key.sh ~/git/init/id_rsa.aes256
 
 git remote remove origin
-git remote add gitee git@gitee.com:searchstar/myetc.git
-
-
-cd ~/.ssh
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-yes | ssh -T git@gitee.com
+git remote add origin git@gitee.com:searchstar/myetc.git
 
 cd ~/git
 git clone git@gitee.com:searchstar/tools.git
-cd tools
-git remote rename origin gitee
-
-ln -s ~/git/tools/os ~
-ln -s ~/git/tools/os/linux ~
-
-cp ~/linux/config/ssh_config.txt ~/.ssh/config
-chmod 600 ~/.ssh/config
-
-cd ~/git
-bash ~/os/Init/git.sh
-
-#cd ~
-#mkdir log
