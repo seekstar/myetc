bfc021d3c1dbdfe17a630696f9fd95611b1c0c9b
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-05 12:50:42 +0800

    git config --global pull.ff only

--- public.sh
+++ public.sh
@@ -1,6 +1,7 @@
 # Since this repository has been cloned, git should have been installed.
 git config --global user.email "jianshengqiu.cs@gmail.com"
 git config --global user.name "Jiansheng Qiu"
+git config --global pull.ff only
 
 if [ ! -d ~/.profile.d ]; then
 	mkdir ~/.profile.d
