92052f340c2dc23d5ddb57fbd812f72eec652348
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-11-29 22:51:05 +0800

    set-upstream

--- private.sh
+++ private.sh
@@ -10,6 +10,7 @@ bash helper/extract_key.sh ~/git/init/id_rsa.aes256
 
 git remote remove origin
 git remote add origin git@gitee.com:searchstar/myetc.git
+git pull origin master --set-upstream
 
 cd ~/git
 git clone git@gitee.com:searchstar/tools.git
