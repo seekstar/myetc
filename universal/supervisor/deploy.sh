40c8f38e3a54d99eeb75d7c59dbc86f52d513e0e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-12-11 20:58:43 +0800

    ~/.local/supervisor/log

--- universal/supervisor/deploy.sh
+++ universal/supervisor/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 pip3 install supervisor
-mkdir -p ~/.local/supervisor
+mkdir -p ~/.local/supervisor/log
 mkdir -p ~/.config/supervisor.d
 sed "s,__home__,$HOME,g" supervisord.conf > ~/.config/supervisord.conf
 echo "supervisord -c $HOME/.config/supervisord.conf" > ~/.rc.d/supervisor.sh
