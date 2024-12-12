2a24de3131d437e1a4f88f9f9d90d8b848be30de
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-12-12 05:17:45 +0000

    Clean up

--- universal/supervisor/deploy.sh
+++ universal/supervisor/deploy.sh
@@ -5,5 +5,4 @@ mkdir -p ~/.config/supervisor.d
 sed "s,__home__,$HOME,g" supervisord.conf > ~/.config/supervisord.conf
 echo "supervisord -c $HOME/.config/supervisord.conf" > ~/.rc.d/supervisor.sh
 sh ~/.rc.d/supervisor.sh
-echo 'alias mysupervisor="supervisorctl -c ~/.config/supervisord.conf"' > ~/.profile.d/mysupervisor.sh
 ln -s $(pwd)/mysupervisor ~/.local/bin
