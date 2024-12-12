cbda094b267cc296be9c4488cff3503b52b21c86
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-12-12 05:19:36 +0000

    Fix

--- universal/supervisor/deploy.sh
+++ universal/supervisor/deploy.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.config/supervisor.d
 sed "s,__home__,$HOME,g" supervisord.conf > ~/.config/supervisord.conf
 echo "supervisord -c $HOME/.config/supervisord.conf" > ~/.rc.d/supervisor.sh
 sh ~/.rc.d/supervisor.sh
-ln -s $(pwd)/mysupervisor ~/.local/bin
+ln -s $(pwd)/mysupervisor ~/.local/bin/
