2bb0e02e36dba7843dc4a4f2dbc9d8730555504e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-01-18 21:20:34 +0800

    Update NixOS config

--- config/configuration.nix
+++ config/configuration.nix
@@ -6,11 +6,11 @@
 
 {
 	imports = [
-		./desktop-environment.nix
 		# Include the results of the hardware scan.
 		./hardware-configuration.nix
 	];
 
+	boot.loader.efi.efiSysMountPoint = "/boot/efi";
 	nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
 
 	# Define a user account. Don't forget to set a password with ‘passwd’.
@@ -26,7 +26,6 @@
 	boot.loader.efi.canTouchEfiVariables = true;
 	boot.loader.grub = {
 		enable = true;
-		version = 2;
 		device = "nodev";
 		efiSupport = true;
 		useOSProber = true;
@@ -37,60 +36,56 @@
 
 	networking.networkmanager.enable = true;
 
-	# Configure network proxy if necessary
-	#networking.proxy.default = "http://127.0.0.1:8234/";
-	#networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	nixpkgs.config.allowUnfree = true;
 
-	# Enable sound.
-	sound.enable = true;
-	hardware.pulseaudio.enable = true;
-
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
 	environment.systemPackages = with pkgs; [
 		python3
-		# JRE is needed by nutstore.
-		openjdk
-		vim # The Nano editor is also installed by default.
-		wget
-		firefox
+		vim
 		git
 		openssl
 		trash-cli
+		python312Packages.supervisor
+
 		konsole
-		element-desktop
 		seafile-client
-		nodePackages.npm
-		nodejs
-		nixos.python310Packages.pip
-		iotop
-		jnettop
-		gnome.gnome-system-monitor
+		seafile-shared
+		gedit
+		firefox
+		vscode
+		keepassxc
+		element-desktop
+		gnome-system-monitor
 	];
 
 	# List services that you want to enable:
 
-	# Enable the X11 windowing system.
-	services.xserver.enable = true;
-
 	# Enable touchpad support (enabled default in most desktopManager).
-	services.xserver.libinput.enable = true;
+	services.libinput.enable = true;
 
 	# Enable CUPS to print documents.
 	services.printing.enable = true;
 
 	# Enable the OpenSSH daemon.
 	services.openssh.enable = true;
-	services.flatpak.enable = true;
+
+	# Enable the X11 windowing system.
+	services.xserver.enable = true;
+
+	services.displayManager.sddm.enable = true;
+	services.desktopManager.plasma6.enable = true;
 
 	i18n.inputMethod = {
-		enabled = "fcitx5";
-		fcitx.engines = with pkgs.fcitx-engines; [ rime ];
-		fcitx5.enableRimeData= true;
+		enable = true;
+		type = "fcitx5";
 		fcitx5.addons = with pkgs; [
+			rime-data
 			fcitx5-rime
 		];
 	};
+
+	xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
+	xdg.portal.enable = true;
+	services.flatpak.enable = true;
 
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
@@ -99,6 +94,5 @@
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 	system.stateVersion = "22.05"; # Did you read the comment?
-
 }
 
