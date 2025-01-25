dea4f9dd57d214008b193d8e6a5ec4f671bbaf92
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-01-25 13:46:35 +0800

    Remove reflector

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -1,8 +1,2 @@
-#sudo reflector --country China --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-#sudo mv /etc/xdg/reflector/reflector.conf /etc/xdg/reflector/reflector.conf.$(date +%Y%m%d%X%N)
-#sudo cp config/reflector.conf /etc/xdg/reflector/
-#sudo systemctl enable reflector.timer
-#sudo systemctl start reflector.timer
-
 bash pacman.sh
 bash public.sh
