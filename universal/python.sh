d13325b5ddd057857484095fd86e1e4fa35809b7
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-02-17 20:17:36 +0800

    Fix

--- universal/python.sh
+++ universal/python.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 python3 -m venv ~/.venvs/base
-echo ". ~/.venvs/base/bin/activate" >> ~/.profile.d/python.sh
+if [ ! -f ~/.profile.d/python.sh ]; then
+	echo ". ~/.venvs/base/bin/activate" > ~/.profile.d/python.sh
+fi
 . ~/.venvs/base/bin/activate
 
 # UpgradePrompt will print horrible traceback on NixOS
 pip3 config set global.disable-pip-version-check true
+
+if ! command -v trash-put; then
+	pip3 install trash-cli
+fi
