0735e028985e6e895d2116b5c9e64c051990f69d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-05-17 05:59:55 +0000

    Update

--- admin/nix.sh
+++ admin/nix.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # https://seekstar.github.io/2023/02/04/使用国内源安装和使用nix包管理器/
 
 # https://mirrors.tuna.tsinghua.edu.cn/help/nix/
@@ -5,7 +6,7 @@ sh <(curl https://mirrors.tuna.tsinghua.edu.cn/nix/latest/install) --daemon --no
 source /etc/profile
 
 # https://mirrors.tuna.tsinghua.edu.cn/help/nix/
-sudo bash -c "echo substituters = https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org/ >> /etc/nix/nix.conf"
+sudo sh -c "echo substituters = https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org/ >> /etc/nix/nix.conf"
 sudo -i nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
 sudo -i nix-channel --update
 nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
