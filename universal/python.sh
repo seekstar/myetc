2134c273d7f95df0365c51b1b4cc092fa0927bee
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-11-10 23:01:37 +0800

    Update

--- universal/python.sh
+++ universal/python.sh
@@ -8,6 +8,6 @@ fi
 # UpgradePrompt will print horrible traceback on NixOS
 pip3 config set global.disable-pip-version-check true
 
-if ! command -v trash-put; then
+if ! command -v trash-put > /dev/null; then
 	pip3 install trash-cli
 fi
